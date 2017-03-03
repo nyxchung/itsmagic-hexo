50dbaf877faad9d8507d1c236bda105c2457aaff
NyxChung <neonyx@naver.com>
2017-03-03 03:41:09 -0500

    Site updated: 2017-03-03 03:41:09

--- js/script.js
+++ js/script.js
@@ -1,96 +1,52 @@
-(function ($, window) {
- 
-    var App = App || {
-        init: function () {
-            $('.js-toggle-search').on('click', function () {
-                $('.js-search').toggleClass('is-visible');
-            });
- 
-            $('.js-next a').on('click', function (e) {
-                $(infinite_scroll.contentSelector).infinitescroll(infinite_scroll);
- 
-                var $body = $('body');
- 
-                $body.scrollTop($body.scrollTop() - 1);
- 
-                e.preventDefault();
-            })
- 
-            $(window).keydown(function (event) {
-                if (event.keyCode == 27) {
-                    if ($('.js-search').attr('class').indexOf('is-visible') > 0) {
-                        $('.js-search').removeClass('is-visible');
-                    }
-                }
-            });
- 
-            $('.js-search .text-input').keydown(function (event) {
-                if (event.keyCode == 13) {
-                    location.href = 'https://www.google.com/search?q=site:yumemor.com ' + $(this).val();
-                    return false;
-                }
-            })
-        }
-    };
- 
+(function($){
+  
+  // Caption
+  $('.article-entry').each(function(i){
+    $(this).find('img').each(function(){
+      if ($(this).parent().hasClass('fancybox')) return;
 
-    $(App.init);
+      var alt = this.alt;
 
-    $(function(){
- 
-        var navToggle = $('#nav-toggle'),
-            nav = $('nav'),
-            navLinks = nav.find('a');
- 
-        navToggle.on('click', function () {
-            navToggle.toggleClass('active');
-            nav.toggleClass('open');
-            return false;
-        });
-        navLinks.on('click', function () {
-            navToggle.toggleClass('active');
-            nav.toggleClass('open');
-        });
- 
-        $(document).on('click', function () {
-            if (nav.hasClass('open')) {
-                navToggle.toggleClass('active');
-                nav.toggleClass('open');
-            }
-        });
- 
-        $('.btn-slide').click(function () {
-            $('#panel').slideToggle("slow");
-            $(this).toggleClass("active");
-            return false;
-        });
- 
-        $(window).scroll(function () {
-            var header = $('header');
- 
-            if ($(this).scrollTop() > 1) {
-                header.addClass("scrolled");
-            } else {
-                header.removeClass("scrolled");
-            }
-        });
- 
-        $("#social-share").click(function () {
-            $("#social").toggleClass("visible").slideToggle(200);
-        });
- 
-        if ($('.welcome')[0]) {
-            $('.author-info').hide();
-            $('span.info-edit').click(function () {
-                $('.author-info').toggle();
-            });
-        }
+      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-        var bannerNode = $('.top-image');
-        if(bannerNode.data('enable')){
-            var index = parseInt((Math.random() * 4) + 1);
-            bannerNode.attr('style','background-image:url(/banner/'+index+'.jpg)');
-        }
-    })
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+    });
 
-}(jQuery, window));
+    $(this).find('.fancybox').each(function(){
+      $(this).attr('rel', 'article' + i);
+    });
+  });
+
+  if ($.fancybox){
+    $('.fancybox').fancybox();
+  }
+
+  // Mobile nav
+  var $container = $('#container'),
+    isMobileNavAnim = false,
+    mobileNavAnimDuration = 200;
+
+  var startMobileNavAnim = function(){
+    isMobileNavAnim = true;
+  };
+
+  var stopMobileNavAnim = function(){
+    setTimeout(function(){
+      isMobileNavAnim = false;
+    }, mobileNavAnimDuration);
+  }
+
+  $('#main-nav-toggle').on('click', function(){
+    if (isMobileNavAnim) return;
+
+    startMobileNavAnim();
+    $container.toggleClass('mobile-nav-on');
+    stopMobileNavAnim();
+  });
+
+  $('#wrap').on('click', function(){
+    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
+
+    $container.removeClass('mobile-nav-on');
+  });
+})(jQuery);
